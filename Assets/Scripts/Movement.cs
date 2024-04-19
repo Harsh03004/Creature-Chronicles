12125568917a0e081973065b1cdbdb710aa46587
yxrtoze <manasvig7700@gmail.com>
2024-04-19 17:31:19 +0530

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = 10f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // Initialize anim with the Animator component of this GameObject
+    }
+
+    void FixedUpdate()
+    {
+        // Check for continuous key presses using GetKey instead of GetKeyDown
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("Walk", true);
+            // Make sure "Back" is false when moving forward or sideways
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+    }
+}
